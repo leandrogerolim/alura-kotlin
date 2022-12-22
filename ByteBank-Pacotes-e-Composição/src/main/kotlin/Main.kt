1ab52f553d5f7b35e22b5b4a67e726d7e5fda5b0
Leandro <leandrogerolim@gmail.com>
2022-12-22 19:22:40 -0400

    companion object conta. posso alterar o contador sem perder o valor inicial.

--- ByteBank-Pacotes-e-Composição/src/main/kotlin/Main.kt
+++ ByteBank-Pacotes-e-Composição/src/main/kotlin/Main.kt
@@ -1,12 +1,21 @@
 import br.com.alura.bytebank.modelo.Conta
+import br.com.alura.bytebank.modelo.Endereco
 
 
 fun main() {
 
     println("Bem vindo ao Bytebank")
-testaContasDiferentes()
-    testaCopiasEReferencias()
+    val endereco = Endereco()
 
     println(Conta.total)
+    val objeto: Any = Any()
 
+    imprime(objeto)
+
+    val teste: Any = imprime(endereco)
+    println(teste)
+}
+    fun imprime(valor: Any) : Any{
+        println(valor)
+        return valor
 }
